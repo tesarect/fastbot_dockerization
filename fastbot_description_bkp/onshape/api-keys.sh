715e428bdfe2c174d196a211ff8607f638e83933
tesarect <karthikn.balasubramanian@gmail.com>
2026-05-19 12:24:09 +0530

    remove scratch and backup files

--- fastbot_description_bkp/onshape/api-keys.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Replace access and secret key with your own, copied from the popup once API key was created in onshame:
-# The API key's access key is The API key's access key is ... and the secret key is ... Please transfer this securely to your application now as you will not be able to display this secret key string again. 
-export ONSHAPE_API=https://cad.onshape.com
-export ONSHAPE_ACCESS_KEY=on_yYtwktmq7bundmXgcgbdv
-export ONSHAPE_SECRET_KEY=0cfpw1wSGoMEhXqrRMZzllrN2q5Eyr4VWfZIppX5IrKGL4ek
